9894b3e6b0394749742d948a808c51290379ade1
박재현 <masdf13@naver.com>
2026-07-20 23:02:25 +0900

    Revert "Merge branch 'chore/S15P11E101-229-public-deployment' into 'main'"
    This reverts merge request !16

--- BE_system/src/test/java/com/bbiyong/server/BbiyongServerApplicationTests.java
+++ BE_system/src/test/java/com/bbiyong/server/BbiyongServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.bbiyong.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.datasource.url=jdbc:sqlite:build/test.db")
+@SpringBootTest
 class BbiyongServerApplicationTests {
 
 	@Test
